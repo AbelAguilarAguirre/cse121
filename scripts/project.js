d3a7fb0170a90e1774230de078e5a24105c19713
AbelAguilarAguirre <ulysses.aguilar.aguirre@gmail.com>
2022-10-18 15:03:46 -1000

    Fixed

--- scripts/project.js
+++ scripts/project.js
@@ -22,7 +22,7 @@ async function getMonsters() {
         "https://gist.githubusercontent.com/tkfu/9819e4ac6d529e225e9fc58b358c3479/raw/d4df8804c25a662efc42936db60cfbc0a5b19db8/srd_5e_monsters.json"
     );
     if (response.ok) {
-        const monsters = await response.json();
+        monsters = await response.json();
         output(monsters);
     }
 }
